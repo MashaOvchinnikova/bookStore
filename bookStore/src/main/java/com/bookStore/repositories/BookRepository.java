2a5fb457dc5b337642e3cb7137654f6d3615cf00
masha <mashaovchinnikova01@gmail.com>
2024-04-26 19:07:45 +0500

    Added pagination

--- bookStore/src/main/java/com/bookStore/repositories/BookRepository.java
+++ bookStore/src/main/java/com/bookStore/repositories/BookRepository.java
@@ -1,8 +1,15 @@
 package com.bookStore.repositories;
 
 import com.bookStore.models.Book;
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface BookRepository extends CrudRepository<Book, Integer> {
+@Repository
+@Transactional
+public interface BookRepository extends JpaRepository<Book, Integer> {
+    Page<Book> findAll(Pageable pageable);
 }
