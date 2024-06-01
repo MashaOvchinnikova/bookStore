e1b99d56897122bd1464f2f274e4b0aca7e54404
Hyhyhy1 <103203881+Hyhyhy1@users.noreply.github.com>
2024-06-02 00:26:21 +0500

    -Refactor- File upload now should work

--- bookStore/src/main/java/com/bookStore/controllers/ImageUploadController.java
+++ bookStore/src/main/java/com/bookStore/controllers/ImageUploadController.java
@@ -2,15 +2,20 @@ package com.bookStore.controllers;
 
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.SdkClientException;
+import com.amazonaws.util.IOUtils;
 import com.bookStore.services.PhotoUploadService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.stereotype.Controller;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Controller
 public class ImageUploadController {
@@ -18,7 +23,6 @@ public class ImageUploadController {
     private final PhotoUploadService photoUploadService = new PhotoUploadService();
     @PostMapping("/v1/upload")
     public void Upload(@RequestParam("file") MultipartFile file){
-
         var fileName = file.getOriginalFilename();
         var fileType = file.getContentType();
         try {
@@ -27,8 +31,33 @@ public class ImageUploadController {
             throw new RuntimeException(e);
         }
     }
-    @GetMapping("/v1/download")
-    public void Download(){
 
+    @GetMapping(value = "/v1/download/{fileName}")
+    @ResponseBody
+    public void GetImage(@PathVariable String fileName,  HttpServletResponse response){
+        response.setContentType("image/png");
+        try {
+            var file = photoUploadService.downloadPhoto(fileName);
+            InputStream temp = new ByteArrayInputStream(file);
+            IOUtils.copy(temp, response.getOutputStream());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping("/v1/get_link/{fileName}")
+    public String GetLink(@PathVariable String fileName){
+        try {
+            var link = photoUploadService.getImageLink(fileName);
+            return link;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping("/admin/image_upload")
+    public String imgUploadTest()
+    {
+        return "imgTest";
     }
 }
